72a94a15b28b3f39bd19751497b79c3a69b33868
cholthi <cholthipaul@gmail.com>
2024-03-23 11:12:23 +0200

    intialized the alx-backend-storage repo and first task commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 DROP TABLE IF EXISTS users;
 CREATE table  users (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL,
-        NAME  VARCHAR(255) NOT NULL UNIQUE
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name  VARCHAR(255)
 );
