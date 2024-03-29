ea4064b12d0f98a4742da175b8c987773f092377
cholthi <cholthipaul@gmail.com>
2024-03-29 16:28:38 +0200

    added web.py file

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Fetches a remote url and tracks url hits uisng a redis store"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+
+def count_hits(fn: Callable) -> Callable:
+    """Tracks how many times a url was hit in fn"""
+    store = redis.Redis(password='foobared')
+
+    @wraps(fn)
+    def wrapper(url):
+        key = 'count:{}'.format(url)
+        store.incr(key)
+        result = store.get('cache:{}'.format(url))
+        if result:
+            return result
+        result = fn(url)
+        store.incr('count:{}'.format(url))
+        store.set('cache:{}'.format(url), result, ex=10)
+        return result
+    return wrapper
+
+
+@count_hits
+def get_page(url: str) -> str:
+    """Fetches a remote url content"""
+    return requests.get(url).text
