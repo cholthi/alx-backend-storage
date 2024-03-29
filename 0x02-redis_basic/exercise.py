8cc9b7e32f3dfb4f90d6702e15f7be7cd54beeed
cholthi <cholthipaul@gmail.com>
2024-03-29 15:49:33 +0200

    added the final task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,13 +17,52 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """decorator to store the history of inputs and outputs for
+    a Cache class methods
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush('{}:inputs'.format(
+                method.__qualname__), str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush('{}:outputs'.format(
+                method.__qualname__), output)
+        return output
+    return wrapper
+
+
+def replay(fn: Callable) -> None:
+    """Display the history of calls of a particular function fn"""
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+
+    input_key = '{}:inputs'.format(fn.__qualname__)
+    output_key = '{}:outputs'.format(fn.__qualname__)
+    fn_name = fn.__qualname__
+
+    if redis_store.exists(fn_name) != 0:
+        call_count = int(redis_store.get(fn_name))
+        print('{} was called {} times:'.format(fn_name, call_count))
+    inputs_history = redis_store.lrange(input_key, 0, -1)
+    outputs_history = redis_store.lrange(output_key, 0, -1)
+    for i, o in zip(inputs_history, outputs_history):
+        print('{}(*{}) -> {}'.format(fn_name, i.decode('utf8'), o))
+
+
 class Cache:
     """provides caching using redis store"""
     def __init__(self) -> None:
         """Initializes cache class"""
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(password='foobared')
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generates a key and stores the data in redis"""
